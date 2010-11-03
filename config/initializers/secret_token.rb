ec9f5535efdaa78d79a16400c6b83f8d69d4221f
niczka <dominikarogozinska@gmail.com>
2010-11-03 07:38:42 +0100

    adding, removing, editing and showing announcements work

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tablica::Application.config.secret_token = '84f9a4b2f091606f6d2e54fcc79a01106ae9192527e694eef8398e3eb730831ae2e6c05ed712671a2cb005acd6f1a8943b73263bfe207b15f557732ad19e43b8'
